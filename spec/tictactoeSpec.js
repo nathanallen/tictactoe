0b08a0c9505fd88b796b3310bb7207246b3c6ebc
nathanallen <thenathanator@gmail.com>
2014-03-01 13:00:30 -0800

    test for current symbol and turn

--- spec/tictactoeSpec.js
+++ spec/tictactoeSpec.js
@@ -1,30 +1,42 @@
 ////Tests
 
+var game
+
 describe("The Game Board", function() {
   it("is a 3 x 3 grid", function() {
     var board = new Board()
     start_board = [ ["","",""],  ["","",""],  ["","",""] ]
-    expect(board.grid).toEqual(start_board);
-  });
-});
+    expect(board.grid).toEqual(start_board)
+  })
+})
 
 describe("On My Turn", function() {
+  // var game
+
+  beforeEach(function() {
+    game = new Game()
+  })
+
+  it("the symbol is an X", function(){
+    expect(game.turn).toEqual(0)
+    expect(game.currentPlayerSymbol()).toEqual("X")
+  })
 
   describe("Clicking on an empty cell", function() {
 
     it("shows an X", function() {
       var cell = document.body.querySelector('td')
       cell.click()
-      expect(cell.innerHTML).toEqual("X");
-    });
+      expect(cell.innerHTML).toEqual("X")
+    })
 
     it("updates the grid", function() {
-      var board = new Board();
+      var board = new Board()
       var updated_grid = [ ["X","",""],  ["","",""],  ["","",""] ]
-      expect(board.grid).toEqual(updated_grid);
-    });
+      expect(board.grid).toEqual(updated_grid)
+    })
 
-  });
+  })
 
   describe("Clicking on a taken cell", function() {
 
@@ -32,28 +44,40 @@ describe("On My Turn", function() {
       var cell = document.body.querySelectorAll('td')[8]
       var cell_value_before_click = cell.innerHTML = "Z"
       cell.click()
-      expect(cell.innerHTML).toEqual(cell_value_before_click);
-    });
+      expect(cell.innerHTML).toEqual(cell_value_before_click)
+      cell.innerHTML = ""
+    })
 
-  });
+  })
 
 })
 
 describe("On Your Turn", function() {
+  // var game
+
+  beforeEach(function() {
+    game = new Game()
+    game.turn = 1
+  })
+
+  it("the symbol is an O", function(){
+    expect(game.turn).toEqual(1)
+    expect(game.currentPlayerSymbol()).toEqual("O")
+  })
 
   describe("Clicking on an empty cell", function() {
 
     it("shows an O", function() {
       var cell = document.body.querySelectorAll('td')[1]
       cell.click()
-      expect(cell.innerHTML).toEqual("O");
-    });
+      expect(cell.innerHTML).toEqual("O")
+    })
 
     it("updates the grid", function() {
-      var board = new Board();
+      var board = new Board()
       var updated_grid = [ ["X","O",""],  ["","",""],  ["","",""] ]
-      expect(board.grid).toEqual(updated_grid);
-    });
+      expect(board.grid).toEqual(updated_grid)
+    })
 
   })
 
@@ -63,10 +87,11 @@ describe("On Your Turn", function() {
       var cell = document.body.querySelectorAll('td')[7]
       var cell_value_before_click = cell.innerHTML = "Z"
       cell.click()
-      expect(cell.innerHTML).toEqual(cell_value_before_click);
-    });
+      expect(cell.innerHTML).toEqual(cell_value_before_click)
+      cell.innerHTML = ""
+    })
 
-  });
+  })
 
 })
 
