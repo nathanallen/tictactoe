f1266f07fec7782881cb489e8a1f61c181027aa5
nathanallen <thenathanator@gmail.com>
2014-03-06 22:22:36 -0800

    passing test for three diagonal

--- src/Game.js
+++ src/Game.js
@@ -9,7 +9,7 @@ Game.prototype.evaluate = function(){
   var that = this
   this.checkForWinner(function(we_have_a_winner){
     if (we_have_a_winner){
-      game.over = true
+      //game.over = true
     } else {
       that.nextTurn()
     }
@@ -26,7 +26,7 @@ Game.prototype.currentPlayerSymbol = function(){
 
 Game.prototype.checkForWinner = function(cb){
   var current_grid = this.board.getGrid()
-  if ( this.checkRows(current_grid) || this.checkColumns(current_grid) ){
+  if ( this.checkRows(current_grid) || this.checkColumns(current_grid) || this.checkDiagonals(current_grid) ){
     cb(true)
   } else {
     cb(false)
@@ -34,27 +34,33 @@ Game.prototype.checkForWinner = function(cb){
 }
 
 Game.prototype.checkRows = function(current_grid){
-  var streak = false
-   current_grid.forEach(function(row){
-      if (!(row[0] == "") && row[0] == row[1] && row[1] == row[2]){
-        streak = true
-      }
-    })
-  return streak
+  var that = this
+  current_grid.forEach(function(row){
+    if (row[0] && row[0] == row[1] && row[1] == row[2]){
+      that.over = true
+    }
+  })
+  return this.over
 }
 
 Game.prototype.checkColumns = function(current_grid){
-  var streak = false
   i = 3
   if (i--){
-    if (!(current_grid[0][i] == "") && current_grid[0][i] == current_grid[1][i] && current_grid[1][i] == current_grid[2][i]){
-      streak = true
+    if (current_grid[0][i] && current_grid[0][i] == current_grid[1][i] && current_grid[1][i] == current_grid[2][i]){
+      this.over = true
     }
   }
-   current_grid.forEach(function(row){
-      if (!(row[0] == "") && row[0] == row[1] && row[1] == row[2]){
-        streak = true
-      }
-    })
-  return streak
+  return this.over
+}
+
+Game.prototype.checkDiagonals = function(current_grid){
+  var center = current_grid[1][1]
+  if (center){
+    var opposite_corners1 = (center == current_grid[0][0] && current_grid[0][0] == current_grid[2][2])
+    var opposite_corners2 = (center == current_grid[0][2] && current_grid[0][2] == current_grid[2][0])
+    if (opposite_corners1 || opposite_corners2){
+      this.over = true
+    }
+  }
+  return this.over
 }
