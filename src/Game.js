5aa40f0383680dc5941c08ae18437c47ef950075
nathanallen <thenathanator@gmail.com>
2014-03-06 21:55:40 -0800

    passing test for three in a row

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,15 @@
 function Game(){
   this.players = ["X","O"]
   this.turn = 0
-  this.over = "false"
+  this.over = false
   this.board = new Board()
 }
 
 Game.prototype.evaluate = function(){
   var that = this
-  this.checkForWinner(function(gameover){
-    if (gameover){
-      //
+  this.checkForWinner(function(we_have_a_winner){
+    if (we_have_a_winner){
+      game.over = true
     } else {
       that.nextTurn()
     }
@@ -34,10 +34,11 @@ Game.prototype.checkForWinner = function(cb){
 }
 
 Game.prototype.checkRows = function(current_grid){
-
-  current_grid.forEach(function(row){
-    //impliment this
-    return false
-  })
-
+  var streak = false
+   current_grid.forEach(function(row){
+      if (!(row[0] == "") && row[0] == row[1] && row[1] == row[2]){
+        streak = true
+      }
+    })
+  return streak
 }
